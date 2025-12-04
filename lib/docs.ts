071ef1c75a0ef441632dabe80dd0f91c500c9c99
Abhigyan Trips <contact@abhigyantrips.dev>
2025-12-04 14:52:09 +0530

    chore(ci): update registry config

---
+++ lib/docs.ts
@@ -0,0 +1,3 @@
+export const PAGES_NEW = [];
+
+export const PAGES_UPDATED = [];
--- registry/index.ts
+++ registry/index.ts
@@ -1,11 +1,11 @@
-import { type Registry, RegistryItem, registryItemSchema } from 'shadcn/schema';
+import { type Registry, registryItemSchema } from 'shadcn/schema';
 import { z } from 'zod/v3';
 
 import { hooks } from './hooks/_registry';
 import { lib } from './lib/_registry';
 import { ui } from './ui/_registry';
 
-const DEPRECATED_ITEMS: string[] = [];
+const DEPRECATED_ITEMS = [];
 
 // Shared between index and style for backward compatibility.
 const WIN95_STYLE = {
@@ -17,24 +17,26 @@ const WIN95_STYLE = {
   files: [],
 };
 
+const registryItems = [
+  {
+    name: 'index',
+    ...WIN95_STYLE,
+  },
+  {
+    name: 'style',
+    ...WIN95_STYLE,
+  },
+  ...ui,
+  ...lib,
+  ...hooks,
+].filter((item) => {
+  return !DEPRECATED_ITEMS.includes(item.name);
+});
+
 export const registry = {
   name: 'win95/ui',
   homepage: 'https://win95.abhi.now',
-  items: z.array(registryItemSchema).parse(
-    [
-      {
-        name: 'index',
-        ...WIN95_STYLE,
-      },
-      {
-        name: 'style',
-        ...WIN95_STYLE,
-      },
-      ...ui,
-      ...lib,
-      ...hooks,
-    ].filter((item) => {
-      return !DEPRECATED_ITEMS.includes(item.name);
-    })
-  ),
+  items: z.array(registryItemSchema).parse(registryItems) as z.infer<
+    typeof registryItemSchema
+  >[],
 } satisfies Registry;
